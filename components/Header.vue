7db0ab321d84187fca89beb675a1090fa8c1d998
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-24 19:06:31 +0700

    3 files added, 1 deleted

---
+++ components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div class="diygw-title diygw-top solid-bottom">
+					<div class="title flex1 text-center">หน้าหลัก</div>
+				</div>
+
+</template>
+
+<script setup lang="ts">
+
+
+
+</script>
+
+
+<style>
+.diygw-title {
+  color: white;
+  background: #ff32ad;
+
+}
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from 'components/Header.vue';
+
+
+</script>
